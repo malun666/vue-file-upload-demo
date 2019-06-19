1f0fc9f5ac7923351d227c820038b9409e3702d0
aicoder <malun666@126.com>
2019-06-20 06:57:36 +0800

    upload fixed

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">首页</router-link> |
+      <router-link to="/about">普通上传</router-link>|
+      <router-link to="/mupload">压缩上传</router-link>
     </div>
     <router-view />
   </div>
